63dfe77a1435716ae8139eaf1698f92f0a7b4611
currorincon <145340731+currorincon@users.noreply.github.com>
2026-09-15 14:23:36 +0200

    Añade primer ejercicio de condicionales

---
+++ 02-condicionales/ejercicios/ComprobarNumeroPar.java
@@ -0,0 +1,18 @@
+package condicionales.ejercicios;
+
+import java.util.Scanner;
+
+public class ComprobarNumeroPar {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Introduzca un número: ");
+            int numero = sc.nextInt();
+
+            if (numero % 2 == 0) {
+                System.out.println("El número es par");
+            } else {
+                System.out.println("El número es impar");
+            }
+        }
+    }
+}
