7bda9e462cf0fdfa6f350f0c78d63fd19687481b
currorincon <145340731+currorincon@users.noreply.github.com>
2026-09-15 14:24:39 +0200

    Añade ejercicio representativo de Strings

---
+++ 04-arrays-strings/ejercicios/Palindromo.java
@@ -0,0 +1,15 @@
+package arraysstrings.ejercicios;
+
+import java.util.Scanner;
+
+public class Palindromo {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Introduce una cadena: ");
+            String texto = sc.nextLine().replace(" ", "").toLowerCase();
+
+            String invertida = new StringBuilder(texto).reverse().toString();
+            System.out.println(texto.equals(invertida) ? "Es palíndromo" : "No es palíndromo");
+        }
+    }
+}
