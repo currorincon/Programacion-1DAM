f048739bf82dddc69c6877794d07883419181134
currorincon <145340731+currorincon@users.noreply.github.com>
2026-09-15 14:23:24 +0200

    Añade ejemplo limpio de tipos primitivos

---
+++ 01-fundamentos/Ejemplo02_TiposPrimitivos.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class Ejemplo02_TiposPrimitivos {
+    public static void main(String[] args) {
+        int numEntero = 4 + 6;
+        float numFlotante = 4.0f;
+        double numDouble = 4.8;
+
+        System.out.println("El número entero es " + numEntero);
+        System.out.println("El número flotante es " + numFlotante);
+        System.out.println("El número double es " + numDouble);
+    }
+}
